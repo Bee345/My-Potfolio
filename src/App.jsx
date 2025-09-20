4878d54556777a9a0da02aef8d14b33e5e64bc5a
Bee345 <lilbeezee8@gmail.com>
2025-09-20 17:43:51 +0100

    Changes  By BiiZii

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import './App.css'
+import  './App.css'
+import  './index.css';
 
 const App = () => {
   return (
     <div>
       <h2>Welcome To My Portfolio Development</h2>
-      <h4 className='bg-amber-50 text-4xl text-slate-300'>My Name IS BiiZii</h4>
+      <h4 className='bg-orange-400 text-4xl text-slate-300 w-[80%] m-auto border-8  border-b-fuchsia-700 text-center px-20 py-4 '>My Name IS BiiZii</h4>
 
     </div>
   )
