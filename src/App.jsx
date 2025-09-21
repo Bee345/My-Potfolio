495e7afabbca0d9993de8aeddfafe99b3b33ef79
Bee345 <lilbeezee8@gmail.com>
2025-09-21 17:00:55 +0100

    Changes By BiiZii

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <h2>Welcome To My Portfolio Development</h2>
-      <h4 className='bg-orange-400 text-4xl text-slate-300 w-[80%] m-auto border-8  border-b-fuchsia-700 text-center px-20 py-4 '>My Name IS BiiZii</h4>
+      <h4 className='bg-orange-400 text-4xl font- text-slate-300 w-[80%] m-auto border-8  border-b-fuchsia-700 text-center px-20 py-4 rounded-3xl'>My Name IS BiiZii</h4>
 
     </div>
   )
